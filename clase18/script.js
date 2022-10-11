b01cd65e8423317ce4a6b28823b9f35ee3f395b4
Matamaya <mateoamaya118@gmail.com>
2022-10-11 10:47:49 -0300

    addd clase18

--- clase18/script.js
+++ clase18/script.js
@@ -2,7 +2,7 @@ const arraydePalabra = [];
 
 function addPalabra () {
     //capturo valor input
-   const valuePalabra = document.getElementById('palabra').value;
+   const valuePalabra = document.getElementById('palabra').value;//el .value captura solo el valor del input y no la etiqueta completa
 
    //validacion de que no sea vacio
    if (valuePalabra==='') {
@@ -14,7 +14,7 @@ function addPalabra () {
    //console.log(arraydePalabra);
 
    listarPalabras();
-}
+} 
 
 function listarPalabras(){
     //validar que el ARRAY no sea vacio
@@ -22,14 +22,26 @@ function listarPalabras(){
         return;
     }
 
-    //limpio el div
-    document.getElementById('listado').innerHTML='';
-
+     
     //recorro el array y voy agregando palabras
-    let ul ='<ul>';
+    const ul = document.createElement('ul');//creamos un ul en html con el metodo createelement
     for (let unaPalabra of arraydePalabra){
-        document.getElementById('listado').innerHTML += `${unaPalabra}`;
+        const li = document.createElement('li');
+        li.innerHTML = unaPalabra;//texto dentro del li
+        
+        document.getElementById('listado').innerHTML=('');//limpio el div
+        ul.appendChild(li);//añadir li al ul mediante appendChild
     }
+    
+    document.getElementById('listado').appendChild(ul);
+    //document.getElementById('listado').innerHTML += ul;
 }
+/*
+        li.id = Math.random();// agregar id dinamicamente con js, math random da entre 0 - 0.999999
+        li.className = 'texto-rojo';//crear clase dinamicamente
+        
 
-//variables template
+        //agregar evento dinamico al li
+                li.addEventListener('click',function() {
+            alert('dentro del evento onclick de li')
+        });*/
